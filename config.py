2686447a0306b073df10409dcd2a1c3c6c29e0d6
Bobrick <149104413+Bobrickss@users.noreply.github.com>
2026-04-20 22:23:46 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
+
+# URL где хостится captcha.html
+# Например: https://your-railway-app.up.railway.app
+WEBAPP_URL = os.getenv("WEBAPP_URL", "https://your-railway-app.up.railway.app")
